116745bb6c5a78658502dd7c98d2877e047874dc
Mohammed Nouman <mohammednouman555@gmail.com>
2025-12-24 14:54:48 +0530

    updated models.py

--- models.py
+++ models.py
@@ -1,10 +1,14 @@
-from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, Text
 from database import Base
+from datetime import datetime
 
 class ContactMessage(Base):
     __tablename__="contact_messages"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(100))
-    email = Column(String(100))
-    message = Column(Text)
+    name = Column(String, nullable=False)
+    email = Column(String,nullable=False)
+    message = Column(String, nullable=False)
+
+    is_read = Column(Boolean, default=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
