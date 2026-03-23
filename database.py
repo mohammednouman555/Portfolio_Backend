42f1568dc9a93232b25e972b395e1ebcff4b4d91
Mohammed Nouman <mohammednouman555@gmail.com>
2026-03-23 23:19:20 +0530

    Updated database.py

--- database.py
+++ database.py
@@ -4,14 +4,27 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-if DATABASE_URL and DATABASE_URL.startswith("postgres"):
-    engine = create_engine(DATABASE_URL)
+if DATABASE_URL:
+
+    # Fix postgres:// → postgresql://
+    if DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://")
+
+    engine = create_engine(
+        DATABASE_URL,
+        pool_pre_ping=True
+    )
+
 else:
-    engine = create_engine("sqlite:///./portfolio.db", connect_args={"check_same_thread": False})
+    engine = create_engine(
+        "sqlite:///./portfolio.db",
+        connect_args={"check_same_thread": False}
+    )
 
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
+
 Base = declarative_base()
